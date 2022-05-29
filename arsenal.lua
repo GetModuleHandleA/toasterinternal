c542632a7ec0fb95bdbfc65d262206992912e34c
0x90 <76507977+LordGlizzy@users.noreply.github.com>
2022-05-29 10:55:49 -0700

    Update arsenal.lua

--- arsenal.lua
+++ arsenal.lua
@@ -42,7 +42,7 @@ Client = {
 
 local VLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/vep1032/VepStuff/main/VL"))()
 
-local s = VLib:Window("Toaster Hub", "skidded by delux", "B")
+local s = VLib:Window("Toaster Hub", "Arsenal", "B")
 
 local Combat = s:Tab("Combat")
 local Gunmods = s:Tab("Gun Mods")
